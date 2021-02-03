fb9ee554783f776ad1afa32130fd6d8a18915df6
Damien Heulin <damienheulin87@gmail.com>
2021-02-03 14:35:58 +0100

    Created responsive highlights images

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Fonts
 import '@fontsource/buenard/400.css'
 import '@fontsource/buenard/700.css'
+import '@fontsource/poppins/300.css'
 import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/500.css'
 import '@fontsource/poppins/600.css'
